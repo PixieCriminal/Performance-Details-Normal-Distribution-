3571496be03e78934b2b10cb7955fd30f7be2e2b
Sahasrasmi Yalamanchili <sahasrasmi.yalamanchili@gmail.com>
2021-03-08 15:00:01 +0530

    Create ReadingScore.py

---
+++ ReadingScore.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import random
+import statistics
+import plotly.figure_factory as ff
+df = pd.read_csv("StudentsPerformance.csv")
+data = df['reading score'].tolist()
+mean = sum(data)/len(data)
+std_deviation = statistics.stdev(data)
+median = statistics.median(data)
+mode = statistics.mode(data)
+fig = ff.create_distplot([data], ["Reading Score"])
+fig.show()
